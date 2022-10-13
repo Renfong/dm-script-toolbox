9ed86895347ac761ce94f4e75ead9e141533bc4f
Renfong <windless99@gmail.com>
2022-10-13 23:05:32 +0800

    Update CreateExampleCurves.s

--- CreateExampleCurves.s
+++ CreateExampleCurves.s
@@ -2,53 +2,78 @@
 // Renfong
 // 2022/10/13
 
-number scale = 0.12
+// settings
 string UnitString = "nm"
-number randAmp = 3
+number scale = 0.12
+number ampRandAmp = 0.5
+number cenRandAmp = 2
+number sigRandAmp = 0.5
 
+// function parameter
+string FuntionType
+number amp, cen, sig
+
+void CreateFunctionProfile(image &img, string FuntionType, number amp, number cen, number sig, number scale, string UnitString){
+	img.ImageSetDimensionScale(0,scale)
+	img.ImageSetDimensionUnitString(0,UnitString)
+	
+	cen /= scale
+	sig /= scale
+	if (FuntionType == "Gaussian"){
+		img = amp * exp(-0.5*((icol-cen)/sig)**2)
+	}
+	else if (FuntionType == "erf"){
+		img = amp*0.5*(1+erf((icol-cen)/(sqrt(2)*sig)))
+	}
+	else if (FuntionType == "erfc"){
+		img = amp*(1-0.5*(1+erf((icol-cen)/(sqrt(2)*sig))))
+	}
+	else Throw( "Funtion is not supported." )
+	
+	img += (random()-0.5)*.1
+}
+
+number RandomShift(number randAmp){
+	number randShift = randAmp*(random()-0.5)/0.5
+	
+	return randShift
+}
+
+// main script
 Image img := RealImage("profile",4,500)
-img.ImageSetDimensionScale(0,scale)
-img.ImageSetDimensionUnitString(0,UnitString)
-number amp = 2.9+randAmp*(random()-0.5)/10
-number cen = 178+randAmp*(random()-0.5)
-number sig = 18+randAmp*(random()-0.5)
-img = amp * exp(-0.5*((icol-cen)/sig)**2)
-img += (random()-0.5)*.1
+FuntionType = "erfc"
+amp = 3+RandomShift(ampRandAmp)
+cen = 16+RandomShift(cenRandAmp)
+sig = 3+RandomShift(sigRandAmp)
+CreateFunctionProfile(img, FuntionType, amp, cen, sig, scale, UnitString)
 img.ShowImage()
 ImageDisplay disp = img.ImageGetImageDisplay(0)
-disp.ImageDisplaySetSliceLabelByIndex(0,"N")
+disp.ImageDisplaySetSliceLabelByIndex(0,"Si")
 disp.LinePlotImageDisplaySetSliceDrawingStyle( 0, 1 )
 disp.LinePlotImageDisplaySetSliceComponentColor( 0, 0, 0, 0.3, 0.8 )
 disp.LinePlotImageDisplaySetLegendShown( 1 )
 
 image dummy := img.ImageClone()
-dummy.ImageSetDimensionScale(0,scale)
-dummy.ImageSetDimensionUnitString(0,UnitString)
-amp = 3.5+randAmp*(random()-0.5)/10
-cen = 135+randAmp*(random()-0.5)
-sig = 32+randAmp*(random()-0.5)
-dummy = amp*(1-0.5*(1+erf((icol-cen)/(sqrt(2)*sig))))
-dummy += (random()-0.5)*.1
+FuntionType = "Gaussian"
+amp = 3+RandomShift(ampRandAmp)
+cen = 19.3+RandomShift(cenRandAmp)
+sig = 2.4+RandomShift(sigRandAmp)
+CreateFunctionProfile(dummy, FuntionType, amp, cen, sig, scale, UnitString)
 disp.ImageDisplayAddImage(dummy,"O")
 
 dummy := img.ImageClone()
-dummy.ImageSetDimensionScale(0,scale)
-dummy.ImageSetDimensionUnitString(0,UnitString)
-amp = 2.8+randAmp*(random()-0.5)/10
-cen = 237+randAmp*(random()-0.5)
-sig = 26+randAmp*(random()-0.5)
-dummy = amp * exp(-0.5*((icol-cen)/sig)**2)
-dummy += (random()-0.5)*.1
+FuntionType = "Gaussian"
+amp = 3+RandomShift(ampRandAmp)
+cen = 28.8+RandomShift(cenRandAmp)
+sig = 4.7+RandomShift(sigRandAmp)
+CreateFunctionProfile(dummy, FuntionType, amp, cen, sig, scale, UnitString)
 disp.ImageDisplayAddImage(dummy,"Ti")
 
-
 dummy := img.ImageClone()
-dummy.ImageSetDimensionScale(0,scale)
-dummy.ImageSetDimensionUnitString(0,UnitString)
-amp = 2.92+randAmp*(random()-0.5)/10
-cen = 249+randAmp*(random()-0.5)
-sig = 17+randAmp*(random()-0.5)
-dummy = amp*0.5*(1+erf((icol-cen)/(sqrt(2)*sig)))
-dummy += (random()-0.5)*.1
-disp.ImageDisplayAddImage(dummy,"Si")
+FuntionType = "erf"
+amp = 3+RandomShift(ampRandAmp)
+cen = 35.7+RandomShift(cenRandAmp)
+sig = 3.2+RandomShift(sigRandAmp)
+CreateFunctionProfile(dummy, FuntionType, amp, cen, sig, scale, UnitString)
+disp.ImageDisplayAddImage(dummy,"N")
 disp.LinePlotImageDisplaySetSliceComponentColor( 3, 0, 0.8, 0.3, 0.7 )
